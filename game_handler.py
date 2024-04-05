6cc529442647ea6eeae3e06dbec52283f5033ee1
thilok <thilokloos@gmx.de>
2024-04-05 19:36:25 +0200

    update on game handler

--- game_handler.py
+++ game_handler.py
@@ -3,6 +3,15 @@ from pygame import draw, Rect
 
 grid_color = (128, 128, 128)
 background_color = (0, 0, 0)
+cell_color = (0, 128, 0)
+food_color = (255, 204, 0)
+
+directions = {
+    0: (1, 0),
+    1: (0, 1),
+    2: (-1, 0),
+    3: (0, -1)
+}
 
 
 class GameHandler:
@@ -14,22 +23,100 @@ class GameHandler:
         self.game_id = game_id
 
         self.is_running = True
-        print(self.game_id)
+        self.cells = []
+        self.food_pos = tuple
+        self.is_growing = False
+        self.direction = 0
+        self.game_init()
 
     def draw(self, surface):
-        rect = Rect(self.pos[0], self.pos[1], self.dim[0], self.dim[1])
-        draw.rect(surface, background_color, rect)
-        for i in range(self.grid_size+1):
-            draw.line(surface, grid_color,
-                      (self.pos[0], self.pos[1]+i*self.grid_spacing[1]),
-                      (self.pos[0]+self.dim[0], self.pos[1]+i*self.grid_spacing[1]))
-            draw.line(surface, grid_color,
-                      (self.pos[0] + i * self.grid_spacing[0], self.pos[1]),
-                      (self.pos[0] + i * self.grid_spacing[0], self.pos[1] + self.dim[1]))
+        if self.is_running:
+            rect = Rect(self.pos[0], self.pos[1], self.dim[0], self.dim[1])
+            draw.rect(surface, background_color, rect)
+
+            # drawing cells
+            for cell in self.cells:
+                rect = Rect(self.pos[0]+cell[0]*self.grid_spacing[0], self.pos[1]+cell[1]*self.grid_spacing[1],
+                            self.grid_spacing[0], self.grid_spacing[1])
+                draw.rect(surface, cell_color, rect)
+
+            # drawing food
+            rect = Rect(self.pos[0] + self.food_pos[0] * self.grid_spacing[0],
+                        self.pos[1] + self.food_pos[1] * self.grid_spacing[1],
+                        self.grid_spacing[0], self.grid_spacing[1])
+            draw.rect(surface, food_color, rect)
+
+            # drawing grid
+            for i in range(self.grid_size+1):
+                draw.line(surface, grid_color,
+                          (self.pos[0], self.pos[1]+i*self.grid_spacing[1]),
+                          (self.pos[0]+self.dim[0], self.pos[1]+i*self.grid_spacing[1]))
+                draw.line(surface, grid_color,
+                          (self.pos[0] + i * self.grid_spacing[0], self.pos[1]),
+                          (self.pos[0] + i * self.grid_spacing[0], self.pos[1] + self.dim[1]))
+
+
 
     def step(self):
         if self.is_running:
-            handle_death_conditions()
+            head_pos = self.cells[0]
+            if not self.is_growing:
+                self.cells.pop()
+            else:
+                self.is_growing = False
+
+            self.cells = [(head_pos[0]+directions[self.direction][0], head_pos[1]+directions[self.direction][1])] + self.cells
+
+            self.handle_food_collisions()
+            self.handle_death_conditions()
+
+    def game_init(self):
+        # spawning snake
+        mid = self.grid_size//2
+        self.cells = [(mid, mid), (mid-1, mid), (mid-2, mid)]
+
+        # spawning food
+        self.spawn_food()
 
     def handle_death_conditions(self):
-        pass
+        head_pos = self.cells[0]
+        if head_pos[0] < 0 or head_pos[0] >= self.grid_size or head_pos[1] < 0 or head_pos[1] >= self.grid_size:
+            self.is_running = False
+
+        for cell in self.cells[1:]:
+            if head_pos[0] == cell[0] and head_pos[1] == cell[1]:
+                self.is_running = False
+
+    def handle_food_collisions(self):
+        for cell in self.cells:
+            if self.food_pos[0] == cell[0] and self.food_pos[1] == cell[1]:
+                self.grow_snake()
+                self.spawn_food()
+
+    def change_direction(self, dir_idx: int):
+        if self.is_running and (dir_idx % 2 != self.direction % 2):
+            self.direction = dir_idx
+
+    def overlaps_with_snake(self, pos):
+        for cell in self.cells:
+            if cell[0] == pos[0] and cell[1] == pos[1]:
+                return True
+        return False
+
+    def spawn_food(self):
+        random_pos = (np.random.randint(self.grid_size), np.random.randint(self.grid_size))
+        while self.overlaps_with_snake(random_pos):
+            random_pos = (np.random.randint(self.grid_size), np.random.randint(self.grid_size))
+        self.food_pos = random_pos
+
+    def grow_snake(self):
+        self.is_growing = True
+
+    def get_data(self):
+        cell_map = np.zeros((self.grid_size, self.grid_size), dtype=np.float32)
+        for cell in self.cells:
+            cell_map[cell[1], cell[0]] = 1
+
+
+
+
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pygame
 from game_handler import GameHandler
 
-screen_size = (500, 500)
+screen_size = (800, 800)
 x_padding = 10
 y_padding = 10
-game_grid_size = (3, 3)
+game_grid_size = (1, 1)
+game_size = 10
 
 game_count = game_grid_size[0]*game_grid_size[1]
 
@@ -14,7 +15,7 @@ class App:
     def __init__(self):
         # initiating a clock and setting timer of the application
         self.clock = pygame.time.Clock()
-        self.fps = 30
+        self.fps = 3
         self.time_per_frame = 1000 / self.fps
 
         self._running = True
@@ -36,7 +37,9 @@ class App:
         for i in range(game_grid_size[1]):
             for j in range(game_grid_size[0]):
                 game_pos = ((x_padding+j*(game_dim[0]+x_padding)), (y_padding+i*(game_dim[1]+y_padding)))
-                self.game_handlers.append(GameHandler(game_dim, game_pos, 10, j+i*game_grid_size[0]))
+                self.game_handlers.append(GameHandler(game_dim, game_pos, game_size, j+i*game_grid_size[0]))
+
+        self.display.fill((255, 255, 255))
 
         self.on_execute()
 
@@ -45,16 +48,26 @@ class App:
         if event.type == pygame.QUIT:
             self._running = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                pass
+            if event.key == pygame.K_PLUS:
+                self.fps += 1
+            if event.key == pygame.K_MINUS:
+                self.fps -= 1
+            if event.key == pygame.K_RIGHT:
+                self.game_handlers[0].change_direction(0)
+            if event.key == pygame.K_DOWN:
+                self.game_handlers[0].change_direction(1)
+            if event.key == pygame.K_LEFT:
+                self.game_handlers[0].change_direction(2)
+            if event.key == pygame.K_UP:
+                self.game_handlers[0].change_direction(3)
 
     # loop which will be executed at fixed rate (for physics, animations and such)
     def on_loop(self):
-        pass
+        for game in self.game_handlers:
+            game.step()
 
     # loop which will only be called when enough cpu time is available
     def on_render(self):
-        self.display.fill((255, 255, 255))
 
         for game in self.game_handlers:
             game.draw(self.display)
@@ -67,24 +80,19 @@ class App:
 
     def on_execute(self):
 
-
+        clock = pygame.time.Clock()
         previous = pygame.time.get_ticks()
         lag = 0.0
 
         # game loop
         while self._running:
-            current = pygame.time.get_ticks()
-            elapsed = current - previous
-            lag += elapsed
-            previous = current
-
             for event in pygame.event.get():
                 self.on_event(event)
 
-            while lag > self.time_per_frame:
-                self.on_loop()
-                lag -= self.time_per_frame
+            self.on_loop()
             self.on_render()
+
+            clock.tick(self.fps)
         self.on_cleanup()
 
 
